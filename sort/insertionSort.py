0e8e9eb5ecfeb1564639f8f68cf94b686f3d2bce
simtaewan <86290406+simtaewan@users.noreply.github.com>
2024-10-09 22:26:59 +0900

    Update insertionSort.py

--- sort/insertionSort.py
+++ sort/insertionSort.py
@@ -2,13 +2,13 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 def insertionSort(A, n):
-    global count
     for i in range(1, n):
         newItem = A[i]
         j = i - 1
         while j >= 0 and newItem < A[j]:
             A[j + 1] = A[j]
             j -= 1
-            count += 1
         A[j + 1] = newItem
-        count += 1
+
+insertionSort(arr, n)
+print(arr)
