35eed756211f15e270b0dcafda12e13119441b0e
simtaewan <86290406+simtaewan@users.noreply.github.com>
2024-10-09 22:19:34 +0900

    Create quickSort.py

---
+++ sort/quickSort.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+def quickSort(A, p, r):
+    if p<r:
+        q = partition(A, p, r)
+        quickSort(A, p, q-1)
+        quickSort(A, q+1, r)
+
+def partition(A, p, r):
+    global count
+    x = A[r]
+    i = p-1
+    for j in range(p, r):
+        if A[j]<x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+            count += 1
+    A[i+1], A[r] = A[r], A[i+1]
+    count += 1
+    return i+1
