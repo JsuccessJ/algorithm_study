7777f7e3db50b587fe5217044c4a798e606a1c59
simtaewan <86290406+simtaewan@users.noreply.github.com>
2024-10-11 19:23:17 +0900

    Update select.py

--- sort/select.py
+++ sort/select.py
@@ -1,20 +1,22 @@
-n = int(input())
+n, i = map(int, input().split())
 arr = list(map(int, input().split()))
 
-def select(A, p=0, r=n-1, i):
+def select(A, i, p=0, r=n-1):
     if p == r : return A[p]
     q = partition(A, p, r)
     k = q-p+1
-    if i<k: return select(A, p, q-1, i)
-    elif i==k: return A[q]
-    else: return select(A, q+1, r, i-k)
+    if i < k: return select(A, i, p, q-1)
+    elif i == k: return A[q]
+    else: return select(A, i-k, q+1, r)
 
 def partition(A, p, r):
     x = A[r]
     i = p-1
     for j in range(p, r):
-        if A[j]<x:
+        if A[j]<=x:
             i += 1
             A[i], A[j] = A[j], A[i]
     A[i+1], A[r] = A[r], A[i+1]
     return i+1
+
+print(select(arr, i))
