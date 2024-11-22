e024d57c20aba926dd99b413b0adfb405e60a248
simtaewan <86290406+simtaewan@users.noreply.github.com>
2024-11-22 23:27:01 +0900

    Update LCS.py

--- search/LCS.py
+++ search/LCS.py
@@ -1,10 +1,11 @@
 def LCS(m, n):
-    C = [[0] * (m + 1) for _ in range(n + 1)]
+    C = [[0] * (n+1) for _ in range(m+1)]
     for i in range(1, m+1):
         for j in range(1, n+1):
-            if x[i] == y[i]: C[i][j] = C[i-1][j-1]+1
+            if a[i-1] == b[j-1]: C[i][j] = C[i-1][j-1]+1
             else: C[i][j] = max(C[i-1][j], C[i][j-1])
     return C[m][n]
-            
-x = [1, 5, 3, 2, 1, 6]
-y = [6, 2, 5, 1, 6]
+
+a = input()
+b = input()
+print(LCS(len(a), len(b)))
