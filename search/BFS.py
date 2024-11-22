7077b4ca3e55897fe4ad1b88cbff847e19df3660
simtaewan <86290406+simtaewan@users.noreply.github.com>
2024-11-22 23:27:32 +0900

    Update BFS.py

--- search/BFS.py
+++ search/BFS.py
@@ -1,16 +1,14 @@
 def BFS(G, s):
     visited = [False]*(s-1) + [True] + [False]*(len(G)-s)
     queue=[s]
-    bfs=[]
     while len(queue) :
         u = queue.pop(0)
-        bfs.append(u)
+        print(u, end=' ')
         G[u-1].sort()
         for v in G[u-1]:
             if not visited[v-1]:
                 visited[v-1] = True
                 queue.append(v)
-    for i in bfs: print(i, end=' ')
 
 n=int(input())
 m=int(input())
