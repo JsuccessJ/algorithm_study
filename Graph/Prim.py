dc704c80b28d509ab8485a6cae366e314c603613
simtaewan <86290406+simtaewan@users.noreply.github.com>
2025-04-06 22:13:23 +0900

    Update Prim.py

--- Graph/Prim.py
+++ Graph/Prim.py
@@ -1,3 +1,22 @@
+# Input Format
+# 입력의 첫줄에는 노드의 수(n)과 간선의 정보 수(m)이 들어온다. 노드는 1부터 n까지 있다.
+# 다음 m줄에는 간선을 이루는 노드(x, y)와 그 간선의 가중치 w가 들어온다.
+
+# Output Format
+# u = EXTRACT-MIN(Q)에서 추출되는 u를 순서대로 출력하고 마지막에 MST의 총 가중치의 합을 출력한다.
+# 각 숫자는 빈칸으로 구분한다.
+
+# Sample Input
+# 4 5
+# 1 2 3
+# 1 4 1
+# 2 3 4
+# 2 4 2
+# 3 4 5
+
+# Sample Output
+# 1 4 2 3 7
+
 def Prim(G, r):
     d = [float("inf")] * n
     tree = [None] * n
